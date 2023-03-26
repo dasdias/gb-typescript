74cc4489f6199716cf56e2c1ea07536ddc862fa4
Alexender <dasdias@yandex.ru>
2023-03-26 19:09:11 +0300

    lesson01

--- src/search-form.ts
+++ src/search-form.ts
@@ -1,20 +1,47 @@
 import { renderBlock } from './lib.js'
 
-export function checkDate() {
-	const checkOutDate = document.getElementById('check-out-date') as HTMLInputElement | null;
-	const checkInDate = document.getElementById('check-in-date') as HTMLInputElement | null;
-	const currentDate = new Date(checkOutDate.value);
+export function checkDate(): object {
+  const dateEnter: {
+    checkOutDate: number | null,
+    checkInDate: number | null,
+  } = {
+    checkOutDate: null,
+    checkInDate: null
+  };
+  const checkOutDate = document.getElementById('check-out-date') as HTMLInputElement | null;
+  const checkInDate = document.getElementById('check-in-date') as HTMLInputElement | null;
+  const currentDate = new Date(checkOutDate.value);
 
-
-	console.log(currentDate.getFullYear())
-	// console.log(checkOutDate.value)
+  // dateEnter.checkOutDate = checkOutDate
+  // console.log(checkInDate.value)
+  // console.log(checkOutDate.value)
+  return dateEnter
 }
 
 
 export function renderSearchFormBlock() {
-	renderBlock(
-		'search-form-block',
-		`
+  const currentDate = new Date();
+  // console.log(currentDate.toISOString())
+
+  const datePlusMonth = new Date(currentDate.setMonth(currentDate.getMonth() + 1))
+
+  console.log(formatDate(datePlusMonth))
+  function formatDate(currentDate: Date) {
+
+    function prependZero(number: number | string): string | number {
+      if (number < 9)
+        return "0" + number;
+      else
+        return number;
+    }
+    const year = currentDate.getFullYear()
+    const month = currentDate.getMonth()
+    const date = currentDate.getDate().toString();
+    return `${year}-${prependZero(month)}-${prependZero(date)}`
+  }
+  renderBlock(
+    'search-form-block',
+    `
     <form>
       <fieldset class="search-filedset">
         <div class="row">
@@ -31,7 +58,7 @@ export function renderSearchFormBlock() {
         <div class="row">
           <div>
             <label for="check-in-date">Дата заезда</label>
-            <input id="check-in-date" type="date" value="2021-05-11" min="2021-05-11" max="2021-06-30" name="checkin" />
+            <input id="check-in-date" type="date" value="${formatDate(currentDate)}" min="${formatDate(currentDate)}" max="${formatDate(datePlusMonth)}" name="checkin" />
           </div>
           <div>
             <label for="check-out-date">Дата выезда</label>
@@ -48,5 +75,5 @@ export function renderSearchFormBlock() {
       </fieldset>
     </form>
     `
-	)
+  )
 }
